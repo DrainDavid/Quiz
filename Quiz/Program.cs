bef87b1133ba37fb9e9188f028b6499916e5ad20
David Öjebrink Olssson <David.ojebrinkolsson@elev.ga.ntig.se>
2023-09-26 10:26:44 +0200

    uygffu

---
+++ Quiz/Program.cs
@@ -0,0 +1,51 @@
+﻿int sum = 0;
+Console.WriteLine("いらっしゃいませ to the land of Sweden");
+Console.WriteLine("I never been to Japan but it seems like a good place");
+Console.WriteLine("Now you will do a quiz about  me");
+Console.WriteLine("What is my favorite food");
+console.writeline("1.shushi 2.meatballs 3.pizza");
+
+string quiz1 = Console.ReadLine();
+
+if (quiz1 == "1")
+{
+ sum++;
+ Console.Writeline("よくできました");
+}
+
+Console.writeline("How tall am I?");
+Console.Writeline("1.191cm 2.177cm 3.148cm");
+
+string quiz2 = Console.Readline();
+
+if (quiz2 == "2")
+{
+sum++;
+console.writeline("great job")
+
+}
+
+Consoole.Writeline("what is my favorite hobby");
+Console.writeline("1.Playing games 2.GYM 3.Smeling flowers");
+
+sting quiz3 = Console.Readline();
+
+if (quiz3 == "2")
+{
+sum++;
+Cosole.writeline("you are doing good")
+}
+
+Console.writeline("How many pets do you think i have");
+console.writeline("1.0 2.3 3.1")
+
+string quiz4 = Console.ReadLine();
+
+if (quiz4 == 1)
+{
+    sum++;
+    console.writeline("goof job");
+}
+
+console.writeline("$you got {sum} points")
+Console.ReadLine()
